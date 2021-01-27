27e40862bc909561710917475b1df2197744a8c9
sebastian <seby.stack@s-sch.net>
2021-01-27 13:12:37 +0100

    added more examples

--- working-yaml-config/src/app.controller.ts
+++ working-yaml-config/src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { type } from 'os';
 
 @Controller()
 export class AppController {
@@ -7,6 +8,16 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    if(this.configService.get<boolean>("bool")){
+      return this.configService.get<string>("alternative");
+    }
     return this.configService.get<string>("hello");
+  }
+
+  @Get("t")//test of wrong conversion
+  getWrongConversion():number{
+    let ret = this.configService.get<number>("hello");
+    console.log("typeof ret:",typeof ret);
+    return ret;
   }
 }
