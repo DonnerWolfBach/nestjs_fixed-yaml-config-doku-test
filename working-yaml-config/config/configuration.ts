8fbf558754f27ddcb2cbf3e04fb3ac0f55d5cbac
sebastian <seby.stack@s-sch.net>
2021-01-27 13:07:47 +0100

    configuration.ts: changed type config is casted to

--- working-yaml-config/config/configuration.ts
+++ working-yaml-config/config/configuration.ts
@@ -9,5 +9,5 @@ export default () => {
   return (
     yaml.load(
       readFileSync(join(__dirname, YAML_CONFIG_FILENAME), 'utf8'),
-    ) as Record<string,string>);
+    ) as Record<string,any>);
 };
